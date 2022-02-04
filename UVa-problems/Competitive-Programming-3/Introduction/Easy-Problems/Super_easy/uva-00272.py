feb577c99f967db227a6e3814ca4f49f2e215fbe
David Jose Espinoza Soto <79059139+Daespinoza@users.noreply.github.com>
2022-02-03 21:31:20 -0600

    Create uva-00272.py

---
+++ UVa-problems/Competitive-Programming-3/Introduction/Easy-Problems/Super_easy/uva-00272.py
@@ -0,0 +1,23 @@
+# your code goes here
+A = None
+B = True
+C = False
+D = ""
+
+while B:
+	try:
+		A = input()
+		for a in A:
+			if ( a is "\"") and ( C is False):
+				C = True
+				D = D + '`'+ '`'
+			elif ( a is "\"") and ( C is True):
+				C = False
+				D = D + "\'" + "\'"
+			else:
+				D = D + a
+	except:
+		A = None
+		B = False
+		
+print(D)
