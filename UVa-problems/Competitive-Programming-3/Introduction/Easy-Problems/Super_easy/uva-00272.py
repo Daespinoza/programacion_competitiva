a1545cc0c588ba67b306c9cb7da74594fda04a18
David Jose Espinoza Soto <79059139+Daespinoza@users.noreply.github.com>
2022-02-04 00:27:10 -0600

    Update uva-00272.py

--- UVa-problems/Competitive-Programming-3/Introduction/Easy-Problems/Super_easy/uva-00272.py
+++ UVa-problems/Competitive-Programming-3/Introduction/Easy-Problems/Super_easy/uva-00272.py
@@ -10,14 +10,14 @@ while B:
 		for a in A:
 			if ( a is "\"") and ( C is False):
 				C = True
-				D = D + '`'+ '`'
+				D = D + "`"+ "`"
 			elif ( a is "\"") and ( C is True):
 				C = False
 				D = D + "\'" + "\'"
 			else:
 				D = D + a
+		D = D + "\n"
 	except:
 		A = None
 		B = False
-		
 print(D)
